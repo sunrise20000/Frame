fbf77bd499a2bebedbd91de5a014da27b3f25853
RickySun <sunruihua2008@126.com>
2019-03-20 16:34:03 +0800

    完成了三菱PLC编程口协议的封装，目前只支持Word和Dword，且仅支持D寄存器的读取与写入

---
+++ FXPLCCommunicationLib/Definations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FXPLCCommunicationLib
+{
+    /// <summary>
+    /// 通信只支持D就可以了
+    /// </summary>
+    public enum REGISTER_TYPE
+    {
+      D,
+    }
+    public enum CMD
+    {
+        STX = 0x02,
+        R = 0x30,
+        W = 0x31,
+        ETX = 0x03,
+        ACK = 0x06,
+        NCK = 0x15,
+    };
+}
---
+++ FXPLCCommunicationLibTests/FxPLCTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FXPLCCommunicationLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace FXPLCCommunicationLib.Tests
+{
+    [TestClass()]
+    public class FxPLCTests
+    {
+        [TestMethod()]
+        public void CheckWriteInt()
+        {
+            FxPLC plc = new FxPLC();
+            plc.Open(20);
+            var ret=plc.WriteInt("D100",-987);
+            Console.WriteLine(ret);
+        }
+
+        [TestMethod()]
+        public void CheckReadInt()
+        {
+            FxPLC plc = new FxPLC();
+            plc.Open(20);
+            var ret = plc.ReadInt("D100");
+            Console.WriteLine(ret);
+        }
+
+        [TestMethod()]
+        public void CheckWriteDInt()
+        {
+            FxPLC plc = new FxPLC();
+            plc.Open(20);
+            var ret = plc.WriteDint("D10",-87767556);
+            Console.WriteLine(ret);
+        }
+
+        [TestMethod()]
+        public void CheckReadDInt()
+        {
+            FxPLC plc = new FxPLC();
+            plc.Open(20);
+            var ret = plc.ReadDint("D10");
+            Console.WriteLine(ret);
+        }
+    }
+}
